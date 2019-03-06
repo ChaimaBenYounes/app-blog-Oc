a68d9022bf34fb06b827fd11dc427223deb01dc8
chaimaby <chaima.benyounes1@gmail.com>
2019-03-06 14:10:58 +0100

    [add] bootstrap template and list all the post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostListComponentComponent } from './post-list-component/post-list-component.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostListComponentComponent
+    PostListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/class/post.ts
+++ src/app/class/post.ts
@@ -3,4 +3,11 @@ export class Post {
     content : string; 
     loveIts : number;
     created_at : Date;
+
+    constructor(title: string, content:string, loveIts: number, created_at : Date ) {
+        this.title = title;
+        this.content = content;
+        this.loveIts = loveIts;
+        this.created_at = created_at;
+    }
 }
