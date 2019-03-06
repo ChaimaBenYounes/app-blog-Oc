69cd681b5a33d0de1e932757877a29b1ea25b3d0
chaimaby <chaima.benyounes1@gmail.com>
2019-03-06 14:16:56 +0100

    [refacto] change the date in controller Post

--- src/app/class/post.ts
+++ src/app/class/post.ts
@@ -4,10 +4,10 @@ export class Post {
     loveIts : number;
     created_at : Date;
 
-    constructor(title: string, content:string, loveIts: number, created_at : Date ) {
+    constructor(title: string, content:string, loveIts: number ) {
         this.title = title;
         this.content = content;
         this.loveIts = loveIts;
-        this.created_at = created_at;
+        this.created_at = new Date();
     }
 }
